542d1d2648f592777195b84324cff9d4cc4c7e26
Radiopapus <viktor@zharina.info>
2023-04-07 17:37:25 +0200

    topKFrequent

--- medium/top-k-frequent-elements/option2.kt
+++ medium/top-k-frequent-elements/option2.kt
@@ -2,10 +2,10 @@ class Solution {
     fun topKFrequent(nums: IntArray, k: Int): IntArray {
         val hm = hashMapOf<Int, Int>()
         for (n in nums) {
-            val count = hm.getOrDefault(key, 1)
+            val count = hm.getOrDefault(n, 1)
             hm[n] = count!! + 1
         }
 
-        return hm.toList().sortedByDescending{ (n, count) -> count }.take(k).map { (n, count) -> count }.toIntArray()
+        return hm.toList().sortedByDescending{ (n, count) -> count }.take(k).map { (n, count) -> n }.toIntArray()
     }
 }
